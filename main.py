c4c22f28863c1af71d8aba08ac80600b5eaf81a3
tobias.vetterle <tobias.vetterle@gmail.com>
2023-01-22 20:05:44 +0100

    V1.4 sliders implemented

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ from dash.dependencies import Input, Output
 import dash_bootstrap_components as dbc
 import numpy as np
 
+
+
 # read data
 
 df = pd.read_csv("data.csv", encoding='utf-8-sig', sep=';')
@@ -29,34 +31,43 @@ header = html.H4("Softwaresondierung",
 # checkliste
 check_potential = html.Div(
     [
-        dbc.Label("Wählen Sie die für Sie wichtigen Potentiale aus:"),
+        dbc.Label("Wählen Sie aus der Liste aus, welche Anforderungen die von Ihnen gesuchte Software-Lösung für das kommunale Datenmanagement erfüllen soll:"),
         dcc.Checklist(
             df['Potenzial'].unique(),
             id="check_potential",
             value=["Etablierung", "Nachhaltigkeit"],
-            labelStyle = dict(display='block'), # places each checkbox in a single line
-            inputStyle={"margin-right": "10px"} # creates space between checkbox and label
+            #labelStyle = dict(display='block'), # places each checkbox in a single line, margin-bottom creates space between labels
+            labelStyle = {"display": "block", "margin-bottom": "15px"},
+            inputStyle={"margin-right": "10px"}, # creates space between checkbox and label
         ),
     ], style={
-        'width': '50%',
+        'width': '100%',
         'display': 'inline-block'
     },
     className="mb-4",
 )
 
 # slider
-slider_etablierung = html.Div(
+sliders = html.Div(
     [
-    dcc.Slider(
-        min=0, 
-        max=2, 
-        step=0.2, 
-        value=1, 
-        id='slider_etablierung')
+    dbc.Label("Für ein genaueres Ergebnis können Sie die ausgewählten Anforderungen mit den Schiebereglern gewichten. Fragen Sie sich: 'Wie wichtig ist mir diese Anforderung im Vergleich zu den anderen ausgewählten Anforderungen?'"),
+    dcc.Slider(min=0.2, max=1.8, step=0.8, value=1, marks={0.2: "weniger wichtig", 1: "gleich wichtig", 1.8: "wichtiger"}, id='slider_etablierung'),
+    dcc.Slider(min=0.2, max=1.8, step=0.8, value=1, marks=None, id='slider_benutzerfreundlichkeit'),
+    dcc.Slider(min=0.2, max=1.8, step=0.8, value=1, marks=None, id='slider_anschaffungskosten'),
+    dcc.Slider(min=0.2, max=1.8, step=0.8, value=1, marks=None, id='slider_integration'),
+    dcc.Slider(min=0.2, max=1.8, step=0.8, value=1, marks=None, id='slider_zusammenarbeit'),
+    dcc.Slider(min=0.2, max=1.8, step=0.8, value=1, marks=None, id='slider_datenmengen'),
+    dcc.Slider(min=0.2, max=1.8, step=0.8, value=1, marks=None, id='slider_datenqualitaet'),
+    dcc.Slider(min=0.2, max=1.8, step=0.8, value=1, marks=None, id='slider_datenanalyse'),
+    dcc.Slider(min=0.2, max=1.8, step=0.8, value=1, marks=None, id='slider_rechtemanagement'),
+    dcc.Slider(min=0.2, max=1.8, step=0.8, value=1, marks=None, id='slider_nachhaltigkeit'),
+    dcc.Slider(min=0.2, max=1.8, step=0.8, value=1, marks=None, id='slider_dashboards'),
+    dcc.Slider(min=0.2, max=1.8, step=0.8, value=1, marks=None, id='slider_schnittstellen')
     ], style={
-        'width': '50%',
+        'width': '100%',
         'display': 'inline-block'
     },
+    className="mb-4",
 )
 
 # diagramm
@@ -73,9 +84,17 @@ fig = html.Div(
 # create layout
 
 
-checklist = dbc.Card([check_potential], body=True)
+#checklist = dbc.Card([check_potential], body=True)
+checklist = dbc.Card([check_potential], className="border-0")
 
-slider = dbc.Card([slider_etablierung], body=True)
+sliders = dbc.Card([sliders], className="border-0")
+
+#checklist_and_slider = dbc.CardGroup(
+#    [
+#    dbc.Card([check_potential], body=True),
+#    dbc.Card([slider_etablierung, slider_benutzerfreundlichkeit, slider_anschaffungskosten, slider_integration, slider_zusammenarbeit, slider_datenmengen, slider_datenqualitaet, slider_datenanalyse, slider_rechtemanagement, slider_nachhaltigkeit, slider_schnittstellen], body=True)
+#    ]
+#)
 
 chart1 = dbc.Card([fig], body=True)
 
@@ -87,11 +106,11 @@ app.layout = dbc.Container(
         (
             [
                 dbc.Col([checklist],
-                        width=3,
+                        width=2,
                         #xs=10, sm=8, md=5, lg=6, xl=5,
                         style={"height": "80%"}
                         ),
-                dbc.Col([slider],
+                dbc.Col([sliders],
                         width=3,
                         #xs=10, sm=8, md=5, lg=6, xl=5,
                         style={"height": "80%"}
@@ -112,7 +131,6 @@ app.layout = dbc.Container(
 
 
 # callback function
-# TODO implement sliders for weihging the potentials. Idea: store value of slider input in variable, then mutiply the corresponding line in dffm with it (before aggregating)
 
 @app.callback(
     # keeping the square brackets around the output (inherited from a project with multiple outputs) caused severe problems, because it expected a list of outputs
@@ -120,29 +138,62 @@ app.layout = dbc.Container(
     Output(component_id='radar-graph', component_property='figure'),
     [
         Input(component_id='check_potential', component_property='value'),
-        Input(component_id='slider_etablierung', component_property='value'), #remove this line for working version
+        Input(component_id='slider_etablierung', component_property='value'),
+        Input(component_id='slider_benutzerfreundlichkeit', component_property='value'),
+        Input(component_id='slider_anschaffungskosten', component_property='value'),
+        Input(component_id='slider_integration', component_property='value'),
+        Input(component_id='slider_zusammenarbeit', component_property='value'),
+        Input(component_id='slider_datenmengen', component_property='value'),
+        Input(component_id='slider_datenqualitaet', component_property='value'),
+        Input(component_id='slider_datenanalyse', component_property='value'),
+        Input(component_id='slider_rechtemanagement', component_property='value'),
+        Input(component_id='slider_nachhaltigkeit', component_property='value'),
+        Input(component_id='slider_dashboards', component_property='value'),
+        Input(component_id='slider_schnittstellen', component_property='value'),
     ]
     )
 
-def update_graph(selected_potential, weight_etablierung):
+
+def update_graph(selected_potential, weight_etablierung, weight_benutzerfreundlichkeit, weight_anschaffungskosten, weight_integration,
+                weight_zusammenarbeit, weight_datenmengen, weight_datenqualitaet, weight_datenanalyse, 
+                weight_rechtemanagement, weight_nachhaltigkeit, weight_dashboards, weight_schnittstellen):
+
     dff = df[df['Potenzial'].isin(selected_potential)]
     dff = dff.set_index('Potenzial')
 
-    dff.astype({"spreadsheet": 'float64',
-                "language": 'float64',
-                "bi": 'float64'
+    dff.astype({"Tabellenkalkulationssoftware": 'float64',
+                "Statistische Programmiersprache": 'float64',
+                "Kombinierte Datenbank/BI-Software": 'float64'
                 })
 
     dff = dff.reset_index()
-    dffm = pd.melt(dff, id_vars='Potenzial', value_vars=['spreadsheet', 'language', 'bi'])
+    dffm = pd.melt(dff, id_vars='Potenzial', value_vars=['Tabellenkalkulationssoftware', 'Statistische Programmiersprache', 'Kombinierte Datenbank/BI-Software'])
 
+    # multiply values according to sliders
     dffm["value"] = np.where(dffm["Potenzial"]=="Etablierung", dffm["value"]*weight_etablierung, dffm['value'])
+    dffm["value"] = np.where(dffm["Potenzial"]=="Benutzerfreundlichkeit", dffm["value"]*weight_benutzerfreundlichkeit, dffm['value'])
+    dffm["value"] = np.where(dffm["Potenzial"]=="Anschaffungskosten", dffm["value"]*weight_anschaffungskosten, dffm['value'])
+    dffm["value"] = np.where(dffm["Potenzial"]=="Integration", dffm["value"]*weight_integration, dffm['value'])
+    dffm["value"] = np.where(dffm["Potenzial"]=="Zusammenarbeit", dffm["value"]*weight_zusammenarbeit, dffm['value'])
+    dffm["value"] = np.where(dffm["Potenzial"]=="Datenmengen", dffm["value"]*weight_datenmengen, dffm['value'])
+    dffm["value"] = np.where(dffm["Potenzial"]=="Datenqualität", dffm["value"]*weight_datenqualitaet, dffm['value'])
+    dffm["value"] = np.where(dffm["Potenzial"]=="Datenanalyse", dffm["value"]*weight_datenanalyse, dffm['value'])
+    dffm["value"] = np.where(dffm["Potenzial"]=="Rechtemanagement", dffm["value"]*weight_rechtemanagement, dffm['value'])
+    dffm["value"] = np.where(dffm["Potenzial"]=="Nachhaltigkeit", dffm["value"]*weight_nachhaltigkeit, dffm['value'])
+    dffm["value"] = np.where(dffm["Potenzial"]=="Dashboards", dffm["value"]*weight_dashboards, dffm['value'])
+    dffm["value"] = np.where(dffm["Potenzial"]=="Schnittstellen", dffm["value"]*weight_schnittstellen, dffm['value'])
 
-    #data["10th"] = np.where(data["10th"]<10, data["10th"]*10, data["10th"])
     
     dffg = dffm.groupby(['variable']).agg({'value':'sum'}).reset_index()
 
-    fig = px.line_polar(dffg, r='value', theta='variable', line_close=True)
+    fig = px.line_polar(dffg, r='value', theta='variable', 
+        line_close=True, 
+        title="Welches Software-Tool passt zu meinen Anforderungen?", 
+        template="plotly_white")
+
+#    "labels (dict with str keys and str values (default {})) – By default, column names are used in the figure for axis titles, "
+ #  " legend entries and hovers. This parameter allows this to be overridden. The keys of this dict should correspond to column names, ""
+  # " and the values should correspond to the desired label to be displayed.""
 
     return fig
 
