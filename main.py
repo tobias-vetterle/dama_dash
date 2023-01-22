f7678a82ff6f4f7c0655653534240ee41a19e3c5
tobias.vetterle <tobias.vetterle@gmail.com>
2023-01-22 10:29:08 +0100

    V1.2 began working on sliders

--- main.py
+++ main.py
@@ -31,15 +31,31 @@ check_potential = html.Div(
         dbc.Label("Wählen Sie die für Sie wichtigen Potentiale aus:"),
         dcc.Checklist(
             df['Potenzial'].unique(),
-            inline=True,
             id="check_potential",
             value=["Etablierung", "Nachhaltigkeit"],
+            labelStyle = dict(display='block'), # places each checkbox in a single line
+            inputStyle={"margin-right": "10px"} # creates space between checkbox and label
         ),
     ], style={
         'width': '50%',
         'display': 'inline-block'
     },
     className="mb-4",
+)
+
+# slider
+slider_etablierung = html.Div(
+    [
+    dcc.Slider(
+        min=0, 
+        max=2, 
+        step=0.2, 
+        value=1, 
+        id='slider_etablierung')
+    ], style={
+        'width': '50%',
+        'display': 'inline-block'
+    },
 )
 
 # diagramm
@@ -58,6 +74,8 @@ fig = html.Div(
 
 checklist = dbc.Card([check_potential], body=True)
 
+slider = dbc.Card([slider_etablierung], body=True)
+
 chart1 = dbc.Card([fig], body=True)
 
 
@@ -68,13 +86,18 @@ app.layout = dbc.Container(
         (
             [
                 dbc.Col([checklist],
-                        #width=6,
-                        xs=10, sm=8, md=5, lg=6, xl=5,
+                        width=3,
+                        #xs=10, sm=8, md=5, lg=6, xl=5,
+                        style={"height": "80%"}
+                        ),
+                dbc.Col([slider],
+                        width=3,
+                        #xs=10, sm=8, md=5, lg=6, xl=5,
                         style={"height": "80%"}
                         ),
                 dbc.Col([chart1],
-                        #width=6,
-                        xs=10, sm=8, md=5, lg=6, xl=5,
+                        width=6,
+                        #xs=10, sm=8, md=5, lg=6, xl=5,
                         style={"height": "80%"}
                         ),
             ],
@@ -88,16 +111,19 @@ app.layout = dbc.Container(
 
 
 # callback function
-
+# TODO implement sliders for weihging the potentials. Idea: store value of slider input in variable, then mutiply the corresponding line in dffm with it (before aggregating)
 
 @app.callback(
-    # keeping the square brackets around the output (inherited from a project with multiple outputs) caused severe problems
-    # remember to place / remove the square brackets depending on wether its one or several ouputs...
+    # keeping the square brackets around the output (inherited from a project with multiple outputs) caused severe problems, because it expected a list of outputs
+    # remember to place / remove the square brackets around the output components depending on wether its one or several ouputs...
     Output(component_id='radar-graph', component_property='figure'),
-    Input(component_id='check_potential', component_property='value')
+    [
+        Input(component_id='check_potential', component_property='value'),
+        Input(component_id='slider_etablierung', component_property='value'),
+    ]
     )
 
-def update_graph(selected_potential):
+def update_graph(selected_potential, weight_etablierung):
     dff = df[df['Potenzial'].isin(selected_potential)]
     dff = dff.set_index('Potenzial')
 
@@ -108,6 +134,8 @@ def update_graph(selected_potential):
 
     dff = dff.reset_index()
     dffm = pd.melt(dff, id_vars='Potenzial', value_vars=['spreadsheet', 'language', 'bi'])
+
+
     
     dffg = dffm.groupby(['variable']).agg({'value':'sum'}).reset_index()
 
